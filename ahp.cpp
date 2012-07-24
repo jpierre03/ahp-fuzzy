eee965495810a3361d73c52f45a9794cabc3546e
Jean-Pierre PRUNARET <jean-pierre+git@prunetwork.fr>
2012-07-24 21:33:11 +0200

    add color definitions

--- ahp.cpp
+++ ahp.cpp
@@ -10,7 +10,35 @@
 #include <iostream>
 #include <fstream>
 
+//#define BG_LIGHT
+#ifndef BG_LIGHT
 
+	#define BLACK    "\033[1;30m"
+	#define RED      "\033[1;31m"
+	#define GREEN    "\033[1;32m"
+	#define YELLOW   "\033[1;33m"
+	#define BLUE     "\033[1;34m"
+	#define PURPLE   "\033[1;35m"
+	#define CYAN     "\033[1;36m"
+	#define GREY     "\033[1;37m"
+#else
+	#define BLACK    "\033[0;30m"
+	#define RED      "\033[0;31m"
+	#define GREEN    "\033[0;32m"
+	#define YELLOW   "\033[0;33m"
+	#define BLUE     "\033[0;34m"
+	#define PURPLE   "\033[0;35m"
+	#define CYAN     "\033[0;36m"
+	#define GREY     "\033[0;37m"
+#endif
+
+#define DEFAULT_COLOR "\033[0;m"
+
+#define OK(x,y)		do { printf("%s[%s%s%s]%s   %s%s\n",BLUE,GREEN,"ok",BLUE,DEFAULT_COLOR,x,y); } while (0)
+#define NOK(y)		do { printf("%s[%s%s%s]%s  %s\n",BLUE,RED,"nok",BLUE,DEFAULT_COLOR,y); } while (0)
+
+#define FUNCTION(y)	do { OK("function ",y); } while (0)
+#define OPERATOR(y)	do { OK("function ",y); } while (0)
 
 using namespace std;
 
